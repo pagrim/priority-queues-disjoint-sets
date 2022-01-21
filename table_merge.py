830c0bcbf0079f6997c86703598d0442a1eea093
Peter Grimshaw <pagrimshaw@gmail.com>
2022-01-21 08:03:58 +0000

    Tidied main code, added second test for table merge

--- table_merge.py
+++ table_merge.py
@@ -10,11 +10,11 @@ class TableMerge:
         self.set_tree = DisjointSetTree(len(row_counts))
 
     def merge(self, destination, source):
-        dest_idx, src_idx = destination - 1, source -1
+        dest_idx, src_idx = destination - 1, source - 1
         logging.debug('Candidate merge; destination table %d source table %d', destination, source)
         dest_rows_idx = self.set_tree.find(dest_idx)
         src_rows_idx = self.set_tree.find(src_idx)
-        logging.debug('Calculated data rows indices as destination %d source %d', dest_rows_idx, src_rows_idx)
+        logging.debug('Calculated data rows indices; destination %d source %d', dest_rows_idx, src_rows_idx)
         if dest_rows_idx != src_rows_idx:
             logging.debug('Merging %d with %d', destination, source)
             self.set_tree.union(src_rows_idx, dest_rows_idx)
@@ -22,12 +22,13 @@ class TableMerge:
             logging.debug('Ranks %s', self.set_tree.rank)
             merged_root_idx = self.set_tree.find(src_rows_idx)
             if merged_root_idx == src_rows_idx:
-                merged_non_root_idx = dest_rows_idx
+                self.update_row_counts(src_rows_idx, dest_rows_idx)
             else:
-                merged_non_root_idx = src_rows_idx
-                merged_root_idx = dest_rows_idx
-            self.row_counts[merged_root_idx] += self.row_counts[merged_non_root_idx]
-            self.row_counts[merged_non_root_idx] = 0
+                self.update_row_counts(dest_rows_idx, src_rows_idx)
+
+    def update_row_counts(self, root_index, other_index):
+        self.row_counts[root_index] += self.row_counts[other_index]
+        self.row_counts[other_index] = 0
 
     def merge_fetch_max(self, merge_ops):
         max_rows = []
--- test_table_merge.py
+++ test_table_merge.py
@@ -4,7 +4,8 @@ from table_merge import TableMerge
 
 @pytest.mark.parametrize(("table_rows", "merge_operations", "exp_max_rows"),
 [
-([1, 1, 1, 1, 1], [(3, 5), (2, 4), (1, 4), (5, 4), (5, 3)], [2, 2, 3, 5, 5])
+([1, 1, 1, 1, 1], [(3, 5), (2, 4), (1, 4), (5, 4), (5, 3)], [2, 2, 3, 5, 5]),
+([10, 0, 5, 0, 3, 3], [(6, 6), (6, 5), (5, 4), (4, 3)], [10, 10, 10, 11])
 ])
 def test_merge_fetch_max(table_rows, merge_operations, exp_max_rows):
     tm = TableMerge(row_counts=table_rows)
