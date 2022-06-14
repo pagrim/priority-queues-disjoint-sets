db010d600d1e0f4884a07306531b69a36a1f9e60
Peter Grimshaw <pagrimshaw@gmail.com>
2022-06-14 18:38:45 +0100

    Added main method and adapted to use heapq

--- parallel_processor.py
+++ parallel_processor.py
@@ -1,5 +1,6 @@
-from heap import MinHeap
-from dataclasses import dataclass
+import heapq
+import re
+import sys
 
 class ThreadJob:
 
@@ -27,15 +28,27 @@ class ParallelProcessor:
 
     def __init__(self, jobs, num_threads):
         self.jobs = jobs
-        self.thread_heap = MinHeap([ThreadJob(thread_id=i, start_time=None, finish_time=0) for i in range(num_threads)])
+        self.thread_heap = [ThreadJob(thread_id=i, start_time=None, finish_time=0) for i in range(num_threads)]
+        heapq.heapify(self.thread_heap)
 
     def process(self):
         results = []
         while len(self.jobs) > 0:
             job_processing_time = self.jobs.pop(0)
-            next_thread = self.thread_heap.extract_min()
+            next_thread = heapq.heappop(self.thread_heap)
             new_job_finish_time = job_processing_time + next_thread.finish_time
             results.append(ThreadJob(thread_id=next_thread.thread_id, start_time=next_thread.finish_time, finish_time=new_job_finish_time))
             tj = ThreadJob(thread_id=next_thread.thread_id,start_time=None,finish_time=new_job_finish_time)
-            self.thread_heap.insert(tj)
+            heapq.heappush(self.thread_heap, tj)
         return results
+
+
+if __name__ == '__main__':
+    match_object = re.match(r'(\d+)\s(\d+)', sys.stdin.readline().rstrip())
+    num_threads, num_jobs = int(match_object.group(1)), int(match_object.group(2))
+    jobs = [int(jb) for jb in sys.stdin.readline().rstrip().split(" ")]
+    pp = ParallelProcessor(jobs=jobs, num_threads=num_threads)
+    results = pp.process()
+    for tj in results:
+        print(f"{tj.thread_id} {tj.start_time}")
+
--- test_parallel_processor.py
+++ test_parallel_processor.py
@@ -2,6 +2,7 @@ import pytest
 
 from parallel_processor import ParallelProcessor, ThreadJob
 
+
 def test_less_than():
     tj1 = ThreadJob(*(0, None, 1))
     tj2 = ThreadJob(*(0, None, 2))
@@ -9,9 +10,10 @@ def test_less_than():
     assert tj1 < tj2 and tj1 < tj3 and tj3 < tj2
 
 
-@pytest.mark.parametrize(("jobs", "num_threads", "exp_ids_starts"),[
-([1,2,3,4,5], 2, [(0, 0), (1, 0), (0, 1), (1, 2), (0, 4)]),
-([1]*20, 4, [(id, start) for start_range in [[(0, i), (1, i), (2, i), (3, i)] for i in range(5)] for id, start in start_range])
+@pytest.mark.parametrize(("jobs", "num_threads", "exp_ids_starts"), [
+    ([1, 2, 3, 4, 5], 2, [(0, 0), (1, 0), (0, 1), (1, 2), (0, 4)]),
+    ([1] * 20, 4,
+     [(id, start) for start_range in [[(0, i), (1, i), (2, i), (3, i)] for i in range(5)] for id, start in start_range])
 ])
 def test_process(jobs, num_threads, exp_ids_starts):
     pp = ParallelProcessor(jobs=jobs, num_threads=num_threads)
